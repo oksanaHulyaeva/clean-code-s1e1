8836beca0217d016ca356b33a69a57711a89ab15
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-03-31 22:04:53 +0300

    refactor: complete changing to BEM concept according to rule 2.1 (advanced)

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".button-edit");
-    var containsClass=listItem.classList.contains("task-item-edit-mode");
+    var containsClass=listItem.classList.contains("todo-block__item-edit-mode");
     //If class of the parent is .edit-mode
     if(containsClass){
 
@@ -103,7 +103,7 @@ var editTask=function(){
     }
 
     //toggle .edit-mode on the parent.
-    listItem.classList.toggle("task-item-edit-mode");
+    listItem.classList.toggle("todo-block__item-edit-mode");
 };
 
 
